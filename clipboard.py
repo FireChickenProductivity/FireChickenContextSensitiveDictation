4d8dfeec8709064ee2e587456a482e5b7c5adc21
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-09-10 13:09:13 -0600

    Give credit

--- clipboard.py
+++ clipboard.py
@@ -1,6 +1,8 @@
 from talon import clip, actions
 from .delay_settings import wait_post_copy_delay
 
+#This is based on the get_selected_text action from the community talon repository located here: 
+#https://github.com/talonhub/community
 def get_selected_text():
     with clip.revert():
         actions.edit.copy()
